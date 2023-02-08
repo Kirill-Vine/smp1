492a5e2a365c17ddc070ebdef0d3e4778bc5a249
Kirill Vine <61331094+Kirill-Vine@users.noreply.github.com>
2023-02-08 16:58:01 -0500

    Update Date.java

--- src/Date.java
+++ src/Date.java
@@ -4,8 +4,9 @@ public class Date implements Comparable<Date> {
     private int month;
     private int day;
     public Date() {
-        //bruh idk 0_0
-
+        this.year=Calendar.getInstance().get(Calendar.YEAR);
+        this.month=Calendar.getInstance().get(Calendar.MONTH)+1;
+        this.day=Calendar.getInstance().get(Calendar.DAY_OF_MONTH);
     } //create an object with today’s date (see Calendar class)
     public Date(String date) {
         String[] dateArray = date.split("/",0);
@@ -14,11 +15,25 @@ public class Date implements Comparable<Date> {
             this.day = Integer.parseInt(dateArray[1]);
             this.year = Integer.parseInt(dateArray[2]);
         } catch (NumberFormatException nfe){
-            System.out.println("improper date");
-           return;
+            return;
         }
     }
     //take mm/dd/yyyy and create a Date object
+    private boolean isLeap(){
+        final int QUADRENNIAL = 4;
+        final int CENTENNIAL = 100;
+        final int QUATERCENTENNIAL = 400;
+        if (this.year % QUATERCENTENNIAL == 0) {
+            return true;
+        }
+        if (this.year % CENTENNIAL == 0) {
+            return false;
+        }
+        if (this.year % QUADRENNIAL == 0) {
+            return true;
+        }
+        return  false;
+    }//check if the year is a leap year
     public boolean isValid() {
         final int JANUARY = 1;
         final int FEBRUARY = 2;
@@ -32,20 +47,14 @@ public class Date implements Comparable<Date> {
         final int OCTOBER = 10;
         final int NOVEMBER = 11;
         final int DECEMBER = 12;
-        final int QUADRENNIAL = 4;
-        final int CENTENNIAL = 100;
-        final int QUATERCENTENNIAL = 400;
         final int MONTHS_PER_YEAR = 12;
         final int LONG_MONTHS = 31;
         final int SHORT_MONTHS = 30;
-        final int WEIRD_ONE = 28;
+        final int NON_LEAP_YEAR = 28;
         final int LEAP_YEAR = 29;
-        //idk how to get this :,|
-        //MUST CHANGE =================================================
-        final int CURRENT_YEAR = 2222;
+        final int CURRENT_YEAR = Calendar.getInstance().get(Calendar.YEAR);;
 
         if(this.month > MONTHS_PER_YEAR || this.year > CURRENT_YEAR || this.month < JANUARY){
-            System.out.println("improper date");
             return false;
         }
         switch(this.month){
@@ -56,8 +65,7 @@ public class Date implements Comparable<Date> {
             case AUGUST:
             case OCTOBER:
             case DECEMBER:
-                if(this.day > LONG_MONTHS) {
-                    System.out.println("long month");
+                if(this.day > LONG_MONTHS || this.day<JANUARY){
                     return false;
                 }
                 break;
@@ -66,28 +74,17 @@ public class Date implements Comparable<Date> {
             case SEPTEMBER:
             case NOVEMBER:
                 if(this.day > SHORT_MONTHS){
-                    System.out.println("improper date");
                     return false;
                 }
                 break;
             case FEBRUARY:
-                if (this.year % QUADRENNIAL ==0 ) {
-                    if(this.year % CENTENNIAL == 0) {
-                        if(this.year %QUATERCENTENNIAL == 0) {
-                            if(this.day > LEAP_YEAR) {
-                                System.out.println("improper date");
-                                return false;
-                            }
-                        }
-                    } else {
-                        if(this.day != LEAP_YEAR) {
-                            System.out.println("improper date");
-                            return false;
-                        }
-                    }
-                }
-                if(this.day != WEIRD_ONE) {
-                    System.out.println("improper date");
+                if (this.isLeap()){
+                    if(this.day<=LEAP_YEAR)
+                        return true;
+                    return false;
+                }else{
+                    if(this.day<=NON_LEAP_YEAR){
+                        return true;}
                     return false;
                 }
         }
@@ -123,5 +120,9 @@ public class Date implements Comparable<Date> {
             }
         }
         return -1;
+    }
+    public static void main(String [] args){
+        Date date = new Date("2/29/2023");
+        System.out.println(date.isValid());
     }
 }
